4c7c0fbe1737f8355db527befa3635a394b8d57f
terfulke <terezka.fuleova@gmail.com>
2024-05-26 21:59:36 +0200

    Update visualize_harmonic_analysis.py

--- visualize_harmonic_analysis.py
+++ visualize_harmonic_analysis.py
@@ -1,31 +1,98 @@
 import tkinter as tk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.patches import FancyArrowPatch
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import mido
-from mido import MidiFile
+import pygame
 import threading
-import time
+import random
 
-class ScatterPlotApp:
-    def __init__(self, master):
+def lz77_compress(data, window_size):
+    compressed = []
+    index = 0
+
+    while index < len(data):
+        best_offset = -1
+        best_length = -1 
+        best_match = []
+
+        #search for the longest match in the sliding window
+        for length in range(1, min(len(data) - index, window_size)):
+            subarray = data[index:index + length]
+            offset = find_subarray_match(data, subarray, max(0, index - window_size), index)
+
+            if offset != -1 and length >= best_length:
+                best_offset = index - offset
+                best_length = length
+                best_match = subarray
+
+        if best_match:
+            #add the (offset, length, next_element_index) tuple to the compressed data
+            next_element_index = index + len(best_match) if index + len(best_match) < len(data) else None
+            compressed.append((best_offset, len(best_match), next_element_index))
+            index += len(best_match)
+        else:
+            #no match found, add a zero-offset tuple
+            next_element_index = index + 1 if index + 1 < len(data) else None
+            compressed.append((0, 0, next_element_index))
+            index += 1
+
+    return compressed
+
+def find_subarray_match(array, subarray, start, end):
+    for i in range(start, end):
+        if array[i:i+len(subarray)] == subarray:
+            return i
+    return -1
+
+class MusicPlayer(threading.Thread):
+    def __init__(self, midi_file_path):
+        super().__init__()
+        self.midi_file_path = midi_file_path
+        self.playing = False
+        self.paused = False
+        self.stopped = False
+
+    def run(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load(self.midi_file_path)
+        self.playing = True
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() and not self.stopped:
+            if self.paused:
+                pygame.mixer.music.pause()
+            else:
+                pygame.mixer.music.unpause()
+            pygame.time.Clock().tick(10)
+
+    def stop(self):
+        self.stopped = True
+        pygame.mixer.music.stop()
+
+    def pause(self):
+        self.paused = True
+
+    def unpause(self):
+        self.paused = False
+
+class App:
+    def __init__(self, master, file_name):
         self.master = master
         master.title("Visualisation")
 
-        # Read data from file
-        file = 'turca.txt'
+        self.fig = Figure(figsize=(10, 6), dpi=100)
+        self.ax1 = self.fig.add_subplot(211)
+        self.ax2 = self.fig.add_subplot(212)
+
+        file = file_name + '.txt'
         column_names = ['Order', 'End', 'Chord', 'Root', 'Key', 'Function', 'Sequence', 'File']
         self.df = pd.read_csv(file, sep=':', names=column_names)
         self.df.replace({'null': None, np.nan: None, ' null':None}, inplace=True)
+        self.df['Sequence'] = self.df['Sequence'].apply(lambda x: x.strip('[]').split(', ') if x is not None else None)
 
-        # MIDI file
-        self.midi_file = MidiFile('turca.mid')
-        self.total_ticks = sum([msg.time for msg in self.midi_file if msg.type == 'note_on'])
-        self.ticks_per_beat = self.midi_file.ticks_per_beat
+        self.midi_file_path =  file_name + '.mid'
 
-        # Chord and note mappings
         self.chord_mapping = {'MAJOR_TRIAD': (0, 4, 7), 
                               'MINOR_TRIAD': (0, 3, 7),
                               'AUGMENTED_TRIAD': (0, 4, 8), 
@@ -35,10 +102,10 @@ class ScatterPlotApp:
                               'DIMINISHED_MINOR_SEVENTH': (0, 3, 6, 10), 
                               'MAJOR_SEVENTH': (0, 4, 7, 11), 
                               'MINOR_SEVENTH': (0, 3, 7, 10), 
-                              'AUGMENTED_SEVENTH': (0, 4, 8, 10), 
+                              'AUGMENTED_SEVENTH': (0, 4, 8, 11), 
                               'MINOR_MAJOR_SEVENTH': (0, 3, 7, 11), 
                               'DOMINANT_SEVENTH_INCOMPLETE': (0, 4, 10), 
-                              'DOMINANT_SEVENTH_ALT_INCOMPLETE':(0, 4, 9), 
+                              'DOMINANT_SEVENTH_ALT_INCOMPLETE':(0, 7, 10), 
                               'MAJOR_SEVENTH_INCOMPLETE': (0, 4, 11),
                               'DIMINISHED_SEVENTH_INCOMPLETE': (0, 3, 9),
                               'DIMINISHED_MINOR_SEVENTH_INCOMPLETE': (0, 3, 10),
@@ -59,42 +126,50 @@ class ScatterPlotApp:
 
         key, mood = self.df.loc[self.df['Key'].notna()].iloc[0]['Key'].split()
         self.new_note_mapping = self.shift_note_mapping(key)
-        self.bpm = 120
+        self.tonic_sequence = []
+        self.sequence_marker = []
 
-        # Initialize tkinter window
+        pygame.init()
+        self.music_player = MusicPlayer(self.midi_file_path)
+
         self.create_widgets()
 
-        # Variables for triangle animation
-        self.position = 0
-        self.moving = False
-
     def create_widgets(self):
-        # Create figure and axis for scatter plot
-        self.fig = Figure(figsize=(10, 6), dpi=100)
-        self.ax = self.fig.add_subplot(111)
-
-        # Create canvas for embedding matplotlib figure
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        # Create buttons
-        self.start_button = tk.Button(self.master, text="Start", command=self.start)
-        self.start_button.pack(side=tk.LEFT)
+        self.start_restart_button = tk.Button(self.master, text="Start/Restart", command=self.start_restart_music)
+        self.start_restart_button.pack(side=tk.LEFT, pady=5)
 
-        self.restart_button = tk.Button(self.master, text="Restart", command=self.restart)
-        self.restart_button.pack(side=tk.LEFT)
+        self.pause_unpause_button = tk.Button(self.master, text="Pause/Unpause", command=self.pause_unpause_music)
+        self.pause_unpause_button.pack(side=tk.LEFT, pady=5)
 
-        # Plot MIDI and scatter data
+        self.get_x_value()
         self.plot_data()
-        
-        self.arrow = FancyArrowPatch((0, -1), (0, -0.2), mutation_scale=20, color='black')
-        self.ax.add_patch(self.arrow)
+        self.plot_compression()
 
+    def get_x_value(self):
+        self.x_value = []
+        actual_position = 0
+        previous_order = ''
+        previous_end = 0
+        for index, row in self.df.iterrows():
+            if previous_order == row['Order']:
+                actual_position += (row['End']-previous_end)
+            else:
+                actual_position += row['End']
+            self.x_value.append(actual_position)
+            previous_order = row['Order']
+            previous_end = row['End']
+
+    def get_tonic_sequence(self, mood):
+        if mood == 'MOLL':
+            self.tonic_sequence = [0, 2, 3, 5, 7, 8, 10]
+        elif mood == 'DUR':
+            self.tonic_sequence = [0, 2, 4, 5, 7, 9, 11]
 
     def plot_data(self):
-        self.ax.clear()
-
         tonic_x = []
         tonic_y = []
         for index, row in self.df.iterrows():
@@ -102,7 +177,7 @@ class ScatterPlotApp:
                 key, mood = row['Key'].split()
             if row['Chord'] is None:
                 continue
-            x_value = row['Order'] * 4 + row['End']
+            x_value = self.x_value[index]
             chord_tone, chord_type = row['Chord'].split()
             chord_value = self.chord_mapping.get(chord_type, None)
             if chord_value is not None:
@@ -118,83 +193,104 @@ class ScatterPlotApp:
                 if color == 'green':
                     tonic_x.append(x_value)
                     tonic_y.append(self.new_note_mapping[chord_tone])
-                self.ax.scatter([x_value] * len(y_value), y_value, marker=marker, color=color, s=s)
-        self.ax.set_ylim(-1, 12)
-
-
-        inverse_note_mapping = {}
-        for key, value in self.new_note_mapping.items():
-            if value not in inverse_note_mapping:
-                inverse_note_mapping[value] = [key]
-            else:
-                inverse_note_mapping[value].append(key)
-        y = [f'{order} {note}' for order, note in inverse_note_mapping.items()]
-        self.ax.set_yticks(list(inverse_note_mapping.keys()))
-        self.ax.set_yticklabels(y)
-        self.ax.set_title('Harmonic Functions Analysis Visualization')
-        self.ax.set_xlabel('Time')
-        self.ax.set_ylabel('Harmony') 
-        self.ax.plot(tonic_x, tonic_y, color='lightgreen', linestyle='dashed', linewidth=1)
-
-        self.canvas.draw()
-
-
-    # def stop(self, event=None):
-    #     self.moving = False
-    #     self.playing = False
-
-    def play_midi(self):
-        self.playing = True
-        with mido.open_output() as output:
-            for msg in self.midi_file.play():
-                if not self.playing:
-                    break
-                output.send(msg)
-                # Do not update the arrow position here
-        self.moving = False
-
-    def start(self, event=None):
-        self.moving = True 
-        threading.Thread(target=self.play_midi).start()
-        self.animate_arrow()
-
-    def animate_arrow(self):
-        if self.moving:
-            self.update_arrow_position()
-            self.master.after(10, self.animate_arrow)  # Schedule the method to be called again after 10 milliseconds
-
-    def update_arrow_position(self):
-        self.adjust_animation_speed()
-        self.position += self.animation_speed  # Use adjusted animation speed
-        self.arrow.set_positions((self.position, -1), (self.position, -0.2))
-        self.canvas.draw()
-
-    # def calculate_tempo(self):
-    #     # Calculate tempo of the MIDI file
-    #     try:
-    #         ticks_per_beat = self.midi_file.ticks_per_beat
-    #         tick_times = [msg.time for msg in self.midi_file if msg.type == 'note_on']
-    #         total_ticks = sum(tick_times)
-    #         total_beats = total_ticks / ticks_per_beat
-    #         return (total_beats / (self.midi_file.length / 60.0))  # Tempo in BPM
-    #     except Exception as e:
-    #         print(f"Error calculating tempo: {e}")
-    #         return None
-
-    def adjust_animation_speed(self):
-        # Adjust animation speed dynamically based on tempo
-        target_speed = 1.0  # Baseline speed
-        if self.bpm > 0:
-            target_speed *= 60.0 / self.bpm  # Scale animation speed based on tempo
-        self.animation_speed = target_speed
+                for i in y_value:
+                    if mood != None:
+                        self.get_tonic_sequence(mood)
+                        self.tonic_sequence = [(self.new_note_mapping[key] + i) % 12 for i in self.tonic_sequence]
+                    if i in self.tonic_sequence or self.tonic_sequence == [] or color == 'green' or color == 'yellow' or color == 'blue':
+                        self.ax1.scatter([x_value], i, marker=marker, color=color, s=s)
+                    else:
+                        self.ax1.scatter([x_value], i, marker=marker, color='red', s=s)
             
+            if row['Sequence'] != None:
+                if len(self.sequence_marker) > 0:
+                    if self.df.loc[self.sequence_marker[-1], 'Function'] != row['Function']:
+                        self.sequence_marker.append(index)
+                else:
+                    self.sequence_marker.append(index)
+                if len(self.sequence_marker) > 3 and row['Sequence'][-1] == row['Function']:
+                    x1, x2 = self.x_value[self.sequence_marker[0]], self.x_value[self.sequence_marker[-1]]
+                    rect = plt.Rectangle((x1, 0), x2-x1, 11, linewidth=0.5, edgecolor='black', facecolor='none')
+                    self.ax1.add_patch(rect)
+                    self.sequence_marker = []
 
-    def restart(self, event=None):
-        self.playing = False
-        self.moving = False
-        self.position = 0
-        self.arrow.set_positions((self.position, -1), (self.position, -0.2))
+
+                
+        self.ax1.set_ylim(-1, 12)
+        self.ax1.set_xlim(-5, self.x_value[-1]+5)
+
+        self.inverse_note_mapping = {}
+        for key, value in self.new_note_mapping.items():
+            if value not in self.inverse_note_mapping:
+                self.inverse_note_mapping[value] = [key]
+            else:
+                self.inverse_note_mapping[value].append(key)
+        y = [f'{order} {note}' for order, note in self.inverse_note_mapping.items()]
+        self.ax1.set_yticks(list(self.inverse_note_mapping.keys()))
+        self.ax1.set_yticklabels(y)
+        self.ax1.set_title('Harmonic Functions Analysis Visualization')
+        self.ax1.set_xticks([])
+        self.ax1.set_ylabel('Harmony') 
+        self.ax1.plot(tonic_x, tonic_y, color='orange', linewidth=1)
+
         self.canvas.draw()
+
+    def plot_compression(self):
+        data = []
+        for index, row in self.df.iterrows():
+            cleaned_line = ':'.join(str(value) for value in row.values[1:])
+            data.append(cleaned_line)
+        data.append(None)
+
+        compressed_data = lz77_compress(data, len(data))
+
+        len_data = len(compressed_data)
+        repetitions = dict()
+
+        for i in (compressed_data):
+            offset, length, next_ind = i
+            if next_ind == None:
+                next_ind = len_data
+            if next_ind-offset > next_ind-length: #ensuring that two found repeated segments are distant from each other and have no common elements
+                continue
+            tuple_sequence = tuple(data[next_ind-length: next_ind])
+            if length > 2:
+                if tuple_sequence in repetitions.keys():
+                    repetitions[tuple_sequence].add((next_ind-length, next_ind))
+                    repetitions[tuple_sequence].add((next_ind-offset-length, next_ind-offset))
+                else:
+                    repetitions[tuple_sequence] = {(next_ind-length, next_ind), (next_ind-offset-length, next_ind-offset)}
+                color = (random.uniform(0, 0.7), random.uniform(0, 0.7), random.uniform(0, 0.7))
+                x1_value = [self.x_value[next_ind-length], self.x_value[next_ind-1]]
+                x2_value = [self.x_value[next_ind-offset-length], self.x_value[next_ind-offset-1]]
+                self.ax2.hlines(length, x1_value[0], x1_value[1], color=color, linewidth=3)
+                self.ax2.hlines(length, x2_value[0], x2_value[1], color=color, linewidth=3)
+
+        self.ax2.set_xlim(-5, self.x_value[-1]+5)
+        self.ax2.set_title('Repetitions')
+        self.ax2.set_xlabel('Beats')
+        self.ax2.set_ylabel('Length')
+    
+        self.canvas.draw()
+
+
+    def start_restart_music(self):
+        if not self.music_player.is_alive() or self.music_player.stopped:
+            self.music_player = MusicPlayer(self.midi_file_path)
+            self.music_player.start()
+        else:
+            self.music_player.stop()
+            self.music_player.join()
+            self.music_player = MusicPlayer(self.midi_file_path)
+            self.music_player.start()
+
+    def pause_unpause_music(self):
+        if not hasattr(self, "music_playing") or self.music_playing:
+            pygame.mixer.music.pause()
+            self.music_playing = False
+        else:
+            pygame.mixer.music.unpause()
+            self.music_playing = True
 
     def shift_note_mapping(self, key):
         res_dict = dict()
@@ -203,6 +299,12 @@ class ScatterPlotApp:
             res_dict[key] = (value - shift) % 12
         return res_dict
 
+    def on_close(self):
+        #stop the music player when the window is closed
+        self.music_player.stop()
+        self.master.destroy()
+
 root = tk.Tk()
-app = ScatterPlotApp(root)
+app = App(root, 'turca')
+root.protocol("WM_DELETE_WINDOW", app.on_close)
 root.mainloop()
